84ed4127e7aa2591045dbd33d521bd2e4399fdb5
Daniel Fenner <dfenner@google.com>
2020-07-31 17:11:39 +0200

    cleanup

--- main.py
+++ main.py
@@ -2,13 +2,7 @@ from flask import json
 from flask import request
 from flask import Flask
 
-# If `entrypoint` is not defined in app.yaml, App Engine will look for an app
-# called `app` in `main.py`.
 app = Flask(__name__)
-
-@app.route('/')
-def api_root():
-    return 'hello'
 
 @app.route('/github', methods=['POST'])
 def api_gh_message():
